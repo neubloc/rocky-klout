ae43e38d9bf27f49aed05928d54300485406f25d
keybi <kbackowski@gmail.com>
2011-09-13 11:55:41 +0200

    Adding possibility to pass Twitter ids

--- lib/rocky-klout.rb
+++ lib/rocky-klout.rb
@@ -33,41 +33,48 @@ class RockyKlout
       @@api_key
     end
 
-    def score_klout(usernames)
-      request_uri = "/#{@@api_version}/klout.json?key=#{@@api_key}&users=" + usernames.collect{|name| CGI.escape(name)}.join(",")
+    def score_klout(usernames_or_ids)
+      usernames_or_ids, profile_method = parse_usernames([*usernames_or_ids])
+      request_uri = "/#{@@api_version}/klout.json?key=#{@@api_key}&#{profile_method}=#{usernames_or_ids}"
       self.class.get(@@base_uri + request_uri)
     end
     
-    def user_show(usernames)
-      request_uri = "/#{@@api_version}/users/show.json?key=#{@@api_key}&users=" + usernames.collect{|name| CGI.escape(name)}.join(",")
+    def user_show(usernames_or_ids)
+      usernames_or_ids, profile_method = parse_usernames([*usernames_or_ids])
+      request_uri = "/#{@@api_version}/users/show.json?key=#{@@api_key}&#{profile_method}=#{usernames_or_ids}"
       self.class.get(@@base_uri + request_uri)
     end  
     
-    def user_topics(usernames)
-      request_uri = "/#{@@api_version}/users/topics.json?key=#{@@api_key}&users=" + usernames.collect{|name| CGI.escape(name)}.join(",")
+    def user_topics(usernames_or_ids)
+      usernames_or_ids, profile_method = parse_usernames([*usernames_or_ids])
+      request_uri = "/#{@@api_version}/users/topics.json?key=#{@@api_key}&#{profile_method}=#{usernames_or_ids}"
       self.class.get(@@base_uri + request_uri)
     end
     
-    def user_stats(usernames)
-      request_uri = "/#{@@api_version}/users/stats.json?key=#{@@api_key}&users=" + usernames.collect{|name| CGI.escape(name)}.join(",")
+    def user_stats(usernames_or_ids)
+      usernames_or_ids, profile_method = parse_usernames([*usernames_or_ids])
+      request_uri = "/#{@@api_version}/users/stats.json?key=#{@@api_key}&#{profile_method}=#{usernames_or_ids}"
       self.class.get(@@base_uri + request_uri)
     end
     
-    def user_history(usernames, measure, start_date, end_date)
-      request_uri = "/#{@@api_version}/users/show.json?key=#{@@api_key}&users=" + usernames.collect{|name| CGI.escape(name)}.join(",") +
+    def user_history(usernames_or_ids, measure, start_date, end_date)
+      usernames_or_ids, profile_method = parse_usernames([*usernames_or_ids])
+      request_uri = "/#{@@api_version}/users/show.json?key=#{@@api_key}&#{profile_method}=#{usernames_or_ids}" +
         "&measure=" + CGI.escape(measure) +
         "&start_date=" + CGI.escape(start_date)
         "&end_date=" + CGI.escape(end_date)
       self.class.get(@@base_uri + request_uri)
     end
     
-    def relationship_influenced_by(usernames)
-      request_uri = "/#{@@api_version}/soi/influenced_by.json?key=#{@@api_key}&users=" + usernames.collect{|name| CGI.escape(name)}.join(",")
+    def relationship_influenced_by(usernames_or_ids)
+      usernames_or_ids, profile_method = parse_usernames([*usernames_or_ids])
+      request_uri = "/#{@@api_version}/soi/influenced_by.json?key=#{@@api_key}&#{profile_method}=#{usernames_or_ids}"
       self.class.get(@@base_uri + request_uri)
     end
     
-    def relationship_influencer_of(usernames)
-      request_uri = "/#{@@api_version}/soi/influencer_of.json?key=#{@@api_key}&users=" + usernames.collect{|name| CGI.escape(name)}.join(",")
+    def relationship_influencer_of(usernames_or_ids)
+      usernames_or_ids, profile_method = parse_usernames([*usernames_or_ids])
+      request_uri = "/#{@@api_version}/soi/influencer_of.json?key=#{@@api_key}&#{profile_method}="
       self.class.get(@@base_uri + request_uri)
     end
     
@@ -79,6 +86,18 @@ class RockyKlout
     def topic_verify(topic)
       request_uri = "/#{@@api_version}/topics/verify.json?key=#{@@api_key}&topic=" + CGI.escape(topic)
       self.class.get(@@base_uri + request_uri)
+    end
+    
+    private
+    
+    def parse_usernames(usernames_or_ids)
+      if usernames_or_ids.all? { |username_or_id| username_or_id.is_a? Fixnum }
+        return usernames_or_ids.join(","), "ids"
+      elsif usernames_or_ids.all? { |username_or_id| username_or_id.is_a? String }
+        return usernames_or_ids.collect{|name| CGI.escape(name)}.join(","), "users"
+      else
+        raise "usernames must be an array of strings or integers"
+      end
     end
     
 end
---
+++ rocky-klout.gemspec
@@ -0,0 +1,7 @@
+Gem::Specification.new do |gem|
+  gem.add_dependency 'httparty', '>= 0.6.1'
+  gem.name = 'rocky-klout'
+  gem.require_paths = ['lib']
+  gem.summary = %q{Ruby wrapper for the Klout API}
+  gem.version = '0.0.3'
+end
